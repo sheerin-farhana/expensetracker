f589a28c180d6d57c5e690d3640b2548863baf71
sheerin-farhana <sheerinfarhana25@gmail.com>
2024-01-17 09:04:29 +0530

    removed access logs

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ const { forgotpassword } = require('./controllers/resetpassword');
 
 const app = express();
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 
 
-app.use(morgan('combined',{stream:accessLogStream}));
+// app.use(morgan('combined',{stream:accessLogStream}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
