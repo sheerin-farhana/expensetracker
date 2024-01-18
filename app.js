4655256599cd61cf7b87379f20f1058b5f179765
sheerin-farhana <sheerinfarhana25@gmail.com>
2024-01-18 20:42:11 +0530

    some changes made

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 3000;
 sequelize
     .sync()
     .then(result => {
-        app.listen(PORT, '0.0.0.0', () => {
+        app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
           });
     })
